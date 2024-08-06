9ff43a6b8c3ee354fab39b654a8d6dd8b407c8c6
xkss41 <tanyuxiang.cn@gmail.com>
2024-08-06 23:28:47 +0800

    提取题目到json

--- 地铁.py
+++ 地铁.py
@@ -11,19 +11,21 @@ class map:
             data = json.load(f)
         return data
     def Output(self):
+        num = 0
         for i in range(len(self.bata)):
             print(f"第{i + 1}题，请根据以下提示猜城市：")
-            map.Answer_the_questions(self,i)
-
+            num+=map.Answer_the_questions(self,i)
+            print("你的得分为"+str(num))
     def Answer_the_questions(self,i):
         for j in range(3):
             a = input(self.bata[str(i)][0]["题干"][str(j)] + "\n输入答案或回车揭开提示？")
             if a == self.bata[str(i)][0]["城市"]:
                 print("您揭开了" + str(j + 1) + "个提示，获得了" + str(20 - ((j) * 5)) + "分")
                 print(self.bata[str(i)][0]["解析"])
-                return
+                return (20 - ((j) * 5))
             if j >= 2 and a != self.bata[str(i)][0]["城市"]:
                 print("答题失败")
+                return 0
 
 
     def main(self):
